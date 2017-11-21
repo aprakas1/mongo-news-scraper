6a0601d9b9c46efdccc81d71ff0a594a54a197fc
Arushi Prakash <arushiprakash@hotmail.com>
2017-11-20 23:00:58 -0500

    Working code, ready to submit for homework

---
+++ models/comments.js
@@ -0,0 +1,16 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+	author:{
+		type: String
+	},
+	content: {
+		type: String
+	}
+});
+
+var Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
